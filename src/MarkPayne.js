27d3d0eb7a767c8f099c1ad3f93843f1a8c03512
mpayne30 <142361747+mpayne30@users.noreply.github.com>
2023-09-14 13:33:51 -0500

    Update MarkPayne.js

--- src/MarkPayne.js
+++ src/MarkPayne.js
@@ -5,11 +5,11 @@ import logoConstruction from './resources/logoConstruction.png';
 function MarkPayne() {
 
     const projectData = [
-        { id: projectType, name: 'Project A' },
-        { id: projectLength, name: 'Project B' },
-        { id: estCostLabor, name: 'Project C' },
-        { id: bidPrice, name: 'Project D' },
-        { id: accepted, name: 'Project E' }
+        { id: 1, name: 'projectType' },
+        { id: 2, name: 'projectLength' },
+        { id: 3, name: 'estCostLabor' },
+        { id: 4, name: 'bidPrice' },
+        { id: 5, name: 'accepted' }
       ];
 
     return (
